46289f48d8c8984fc97f2898de32f64b75cb63e4
William Grant <william.grant@canonical.com>
2014-03-14 18:52:15 +1100

    Extract an IntegrationTestMixin.

--- turnip/tests/test_backend_integration.py
+++ turnip/tests/test_backend_integration.py
@@ -18,21 +18,9 @@ from twisted.internet import (
 from turnip.git import GitBackendFactory
 
 
-class TestBackendIntegration(TestCase):
+class IntegrationTestMixin(object):
 
     run_tests_with = AsynchronousDeferredRunTest.make_factory(timeout=30)
-
-    def setUp(self):
-        super(TestBackendIntegration, self).setUp()
-        # Set up a GitBackendFactory on a free port in a clean repo root.
-        self.root = self.useFixture(TempDir()).path
-        self.listener = reactor.listenTCP(0, GitBackendFactory(self.root))
-        self.port = self.listener.getHost().port
-
-    @defer.inlineCallbacks
-    def tearDown(self):
-        super(TestBackendIntegration, self).tearDown()
-        yield self.listener.stopListening()
 
     @defer.inlineCallbacks
     def assertCommandSuccess(self, command, path='.'):
@@ -45,15 +33,12 @@ class TestBackendIntegration(TestCase):
     def test_clone_and_push(self):
         # Test a full clone, commit, push, clone, commit, push, pull
         # cycle using the backend server.
-        yield self.assertCommandSuccess(
-            (b'git', b'init', b'--bare', b'test'), path=self.root)
-        url = b'git://localhost:%d/test' % self.port
         test_root = self.useFixture(TempDir()).path
         clone1 = os.path.join(test_root, 'clone1')
         clone2 = os.path.join(test_root, 'clone2')
 
         # Clone the empty repo from the backend and commit to it.
-        yield self.assertCommandSuccess((b'git', b'clone', url, clone1))
+        yield self.assertCommandSuccess((b'git', b'clone', self.url, clone1))
         yield self.assertCommandSuccess(
             (b'git', b'config', b'user.email', b'test@example.com'),
             path=clone1)
@@ -66,7 +51,7 @@ class TestBackendIntegration(TestCase):
             (b'git', b'push', b'origin', b'master'), path=clone1)
 
         # Re-clone and check that we got the fresh commit.
-        yield self.assertCommandSuccess((b'git', b'clone', url, clone2))
+        yield self.assertCommandSuccess((b'git', b'clone', self.url, clone2))
         out = yield self.assertCommandSuccess(
             (b'git', b'log', b'--oneline', b'-n', b'1'), path=clone2)
         self.assertIn(b'Committed test', out)
@@ -87,6 +72,26 @@ class TestBackendIntegration(TestCase):
     def test_no_repo(self):
         test_root = self.useFixture(TempDir()).path
         output = yield utils.getProcessOutput(
-            b'git', (b'clone', b'git://localhost:%d/test' % self.port),
+            b'git', (b'clone', b'git://localhost:%d/fail' % self.port),
             path=test_root, errortoo=True)
         self.assertIn(b'fatal:', output)
+
+
+class TestBackendIntegration(IntegrationTestMixin, TestCase):
+
+    @defer.inlineCallbacks
+    def setUp(self):
+        super(TestBackendIntegration, self).setUp()
+        # Set up a GitBackendFactory on a free port in a clean repo root.
+        self.root = self.useFixture(TempDir()).path
+        self.listener = reactor.listenTCP(0, GitBackendFactory(self.root))
+        self.port = self.listener.getHost().port
+
+        yield self.assertCommandSuccess(
+            (b'git', b'init', b'--bare', b'test'), path=self.root)
+        self.url = b'git://localhost:%d/test' % self.port
+
+    @defer.inlineCallbacks
+    def tearDown(self):
+        super(TestBackendIntegration, self).tearDown()
+        yield self.listener.stopListening()
