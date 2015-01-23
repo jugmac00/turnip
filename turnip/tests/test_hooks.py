84b4498468e94c02ba03b26a32ca9c6ceffba9d4
William Grant <william.grant@canonical.com>
2015-01-23 17:43:12 +1100

    Use a rules file to forbid writes to particular refs.

--- turnip/tests/test_hooks.py
+++ turnip/tests/test_hooks.py
@@ -6,6 +6,7 @@ from __future__ import (
 
 import os.path
 import subprocess
+import tempfile
 
 from testtools import TestCase
 
@@ -24,30 +25,38 @@ class TestPreReceiveHook(TestCase):
         return '\n'.join(
             b'%s %s %s' % (old, new, ref) for ref, old, new in updates)
 
-    def invokeHook(self, input):
-        hook = subprocess.Popen(
-            [self.hook_path], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
-        stdout, stderr = hook.communicate(input)
+    def invokeHook(self, input, rules):
+        with tempfile.NamedTemporaryFile(mode='wb') as rulefile:
+            rulefile.writelines(rule + b'\n' for rule in rules)
+            rulefile.flush()
+
+            hook = subprocess.Popen(
+                [self.hook_path], stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                env={b'TURNIP_HOOK_REF_RULES': rulefile.name})
+            stdout, stderr = hook.communicate(input)
         return hook.returncode, stdout, stderr
 
-    def assertAccepted(self, updates):
+    def assertAccepted(self, updates, rules):
         self.assertEqual(
-            (0, b'', b''), self.invokeHook(self.encodeRefs(updates)))
+            (0, b'', b''), self.invokeHook(self.encodeRefs(updates), rules))
 
-    def assertRejected(self, updates, message):
+    def assertRejected(self, updates, rules, message):
         self.assertEqual(
-            (1, message, b''), self.invokeHook(self.encodeRefs(updates)))
+            (1, message, b''),
+            self.invokeHook(self.encodeRefs(updates), rules))
 
     def test_accepted(self):
         # A single valid ref is accepted.
         self.assertAccepted(
-            [(b'refs/heads/master', self.old_sha1, self.new_sha1)])
+            [(b'refs/heads/master', self.old_sha1, self.new_sha1)],
+            [])
 
     def test_rejected(self):
         # An invalid ref is rejected.
         self.assertRejected(
             [(b'refs/heads/verboten', self.old_sha1, self.new_sha1)],
+            [b'refs/heads/verboten'],
             b"You can't push to refs/heads/verboten.\n")
 
     def test_rejected_multiple(self):
@@ -56,5 +65,6 @@ class TestPreReceiveHook(TestCase):
             [(b'refs/heads/verboten', self.old_sha1, self.new_sha1),
              (b'refs/heads/master', self.old_sha1, self.new_sha1),
              (b'refs/heads/super-verboten', self.old_sha1, self.new_sha1)],
+            [b'refs/heads/verboten', b'refs/heads/super-verboten'],
             b"You can't push to refs/heads/verboten.\n"
             b"You can't push to refs/heads/super-verboten.\n")
