ad6bb1985f9ec88c8b1140394d6434231ed5a0d5
William Grant <william.grant@canonical.com>
2015-01-23 17:08:22 +1100

    Replace the update hook with pre-receive, to atomically reject the push if any update is bad.

--- turnip/tests/test_hooks.py
+++ turnip/tests/test_hooks.py
@@ -12,37 +12,52 @@ from testtools import TestCase
 import turnip
 
 
-class TestUpdateHook(TestCase):
-    """Tests for the git update hook."""
+class TestPreReceiveHook(TestCase):
+    """Tests for the git pre-receive hook."""
 
     hook_path = os.path.join(
-        os.path.dirname(turnip.__file__), b'data', b'hooks', b'update')
+        os.path.dirname(turnip.__file__), b'data', b'hooks', b'pre-receive')
     old_sha1 = b'a' * 40
     new_sha1 = b'b' * 40
 
-    def assertAccepted(self, ref, old, new):
-        hook = subprocess.Popen(
-            [self.hook_path, ref, old, new],
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = hook.communicate()
-        self.assertEqual(0, hook.returncode)
-        self.assertEqual(b'', stdout)
-        self.assertEqual(b'', stderr)
+    def encodeRefs(self, updates):
+        return '\n'.join(
+            b'%s %s %s' % (old, new, ref) for ref, old, new in updates)
 
-    def assertRejected(self, ref, old, new, message):
+    def assertAccepted(self, updates):
         hook = subprocess.Popen(
-            [self.hook_path, ref, old, new],
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = hook.communicate()
-        self.assertEqual(1, hook.returncode)
-        self.assertEqual(message, stdout)
+            [self.hook_path], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+        stdout, stderr = hook.communicate(self.encodeRefs(updates))
+        self.assertEqual(0, hook.returncode)
         self.assertEqual(b'', stderr)
+        self.assertEqual(b'', stdout)
+
+    def assertRejected(self, updates, message):
+        hook = subprocess.Popen(
+            [self.hook_path], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+        stdout, stderr = hook.communicate(self.encodeRefs(updates))
+        self.assertEqual(1, hook.returncode)
+        self.assertEqual(b'', stderr)
+        self.assertEqual(message, stdout)
 
     def test_accepted(self):
+        # A single valid ref is accepted.
         self.assertAccepted(
-            b'refs/heads/master', self.old_sha1, self.new_sha1)
+            [(b'refs/heads/master', self.old_sha1, self.new_sha1)])
 
     def test_rejected(self):
+        # An invalid ref is rejected.
         self.assertRejected(
-            b'refs/heads/verboten', self.old_sha1, self.new_sha1,
+            [(b'refs/heads/verboten', self.old_sha1, self.new_sha1)],
             b"You can't push to refs/heads/verboten.\n")
+
+    def test_rejected_multiple(self):
+        # A combination of valid and invalid refs is still rejected.
+        self.assertRejected(
+            [(b'refs/heads/verboten', self.old_sha1, self.new_sha1),
+             (b'refs/heads/master', self.old_sha1, self.new_sha1),
+             (b'refs/heads/super-verboten', self.old_sha1, self.new_sha1)],
+            b"You can't push to refs/heads/verboten.\n"
+            b"You can't push to refs/heads/super-verboten.\n")
