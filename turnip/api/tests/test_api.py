7a2211b52580ac497f84493ef552f9afb8454949
Kit Randel <kit.randel@canonical.com>
2015-02-25 19:09:44 +1300

    * Move API tests to api package. * Create TestApp in setUp.

--- turnip/api/tests/test_api.py
+++ turnip/api/tests/test_api.py
@@ -23,20 +23,20 @@ class ApiTestCase(TestCase):
         super(ApiTestCase, self).setUp()
         repo_store = self.useFixture(TempDir()).path
         self.useFixture(EnvironmentVariable("REPO_STORE", repo_store))
+        self.app = TestApp(api.main({}))
         self.repo_path = str(uuid.uuid1())
         self.repo_store = os.path.join(repo_store, self.repo_path)
 
     def test_repo_post(self):
-        app = TestApp(api.main({}))
-        resp = app.post('/repo', json.dumps({'repo_path': self.repo_path}))
+        resp = self.app.post('/repo', json.dumps({'repo_path': self.repo_path}))
         self.assertEquals(resp.status_code, 200)
 
     def test_repo_delete(self):
-        app = TestApp(api.main({}))
-        app.post('/repo', json.dumps({'repo_path': self.repo_path}))
-        resp = app.delete('/repo/{}'.format(self.repo_path))
+        self.app.post('/repo', json.dumps({'repo_path': self.repo_path}))
+        resp = self.app.delete('/repo/{}'.format(self.repo_path))
         self.assertEqual(resp.status_code, 200)
         self.assertFalse(os.path.exists(self.repo_store))
+
 
 if __name__ == '__main__':
     unittest.main()
