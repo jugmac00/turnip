6b1d60e05396c036aab6092068fd536aa9fc055a
William Grant <william.grant@canonical.com>
2015-03-23 20:00:47 +1100

    match_rules takes rule lines, not a rule path.

--- turnip/pack/hooks/pre_receive.py
+++ turnip/pack/hooks/pre_receive.py
@@ -20,11 +20,8 @@ def glob_to_re(s):
         b''.join(b'[^/]*' if c == b'*' else re.escape(c) for c in s))
 
 
-def match_rules(rule_path, ref_lines):
-    with open(rule_path, 'rb') as f:
-        rules = [
-            re.compile(glob_to_re(l.rstrip(b'\n'))) for l in f.readlines()]
-
+def match_rules(rule_lines, ref_lines):
+    rules = [re.compile(glob_to_re(l.rstrip(b'\n'))) for l in rule_lines]
     # Match each ref against each rule.
     errors = []
     for ref_line in ref_lines:
@@ -35,8 +32,9 @@ def match_rules(rule_path, ref_lines):
 
 
 if __name__ == '__main__':
-    errors = match_rules(
-        os.environ[b'TURNIP_HOOK_REF_RULES'], sys.stdin.readlines())
+    with open(os.environ[b'TURNIP_HOOK_REF_RULES'], 'rb') as f:
+        rule_lines = f.readlines()
+    errors = match_rules(rule_lines, sys.stdin.readlines())
     for error in errors:
         print(error)
     sys.exit(1 if errors else 0)
