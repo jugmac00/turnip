f48670225a038aac8ac2015f7a1586711b51f87a
Colin Watson <cjwatson@canonical.com>
2015-04-08 12:47:39 +0100

    Upgrade pygit2 in setup.py as well.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(os.path.join(here, 'README')) as f:
 requires = [
     'cornice',
     'lazr.sshserver',
-    'pygit2>=0.21.0,<0.22.0',
+    'pygit2>=0.22.0,<0.23.0',
     'PyYAML',
     'Twisted',
     'waitress',
