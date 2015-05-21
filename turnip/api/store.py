f2cf63894f8c7ff8efec923d7a2655adcdc79bca
Kit Randel <kit.randel@canonical.com>
2015-05-22 08:48:25 +1200

    * Add repack depth and window. * ensure_config before repack.

--- turnip/api/store.py
+++ turnip/api/store.py
@@ -177,7 +177,7 @@ def delete_repo(repo_path):
 
 
 def repack(repo_path, ignore_alternates=False, single=False,
-           prune=False, no_reuse_delta=False):
+           prune=False, no_reuse_delta=False, window=None, depth=None):
     """Repack a repository with git-repack.
 
     :param ignore_alternates: Only repack local refs (git repack --local).
@@ -185,6 +185,8 @@ def repack(repo_path, ignore_alternates=False, single=False,
     :param prune: Remove redundant packs. (git repack -d)
     :param no_reuse_delta: Force delta recalculation.
     """
+    ensure_config(repo_path)
+
     repack_args = ['git', 'repack', '-q']
     if ignore_alternates:
         repack_args.append('-l')
@@ -194,6 +196,11 @@ def repack(repo_path, ignore_alternates=False, single=False,
         repack_args.append('-d')
     if single:
         repack_args.append('-a')
+    if window:
+        repack_args.append('--window', window)
+    if depth:
+        repack_args.append('--depth', depth)
+
     return subprocess.check_call(
         repack_args, cwd=repo_path,
         stderr=subprocess.PIPE, stdout=subprocess.PIPE)
--- turnip/api/views.py
+++ turnip/api/views.py
@@ -135,16 +135,20 @@ class RepackAPI(BaseAPI):
     @validate_path
     def post(self, repo_store, repo_name):
         repo_path = os.path.join(repo_store, repo_name)
+
         data = extract_json_data(self.request)
         ignore_alternates = data.get('ignore_alternates')
         no_reuse_delta = data.get('no_reuse_delta')
         prune = data.get('prune')
         single = data.get('single')
+        window = data.get('window')
+        depth = data.get('depth')
 
         try:
             store.repack(repo_path, single=single, prune=prune,
                          no_reuse_delta=no_reuse_delta,
-                         ignore_alternates=ignore_alternates)
+                         ignore_alternates=ignore_alternates,
+                         window=window, depth=depth)
         except (CalledProcessError):
             return exc.HTTPInternalServerError()
         return
