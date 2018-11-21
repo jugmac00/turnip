b545f1b6d7e00fab5d76905187130fb1ce803652
Colin Watson <cjwatson@canonical.com>
2018-11-21 12:08:17 +0000

    Add unit tests for HookRPCHandler

---
+++ turnip/pack/tests/fake_servers.py
@@ -0,0 +1,91 @@
+# Copyright 2015-2018 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    )
+
+from collections import defaultdict
+import hashlib
+
+from lazr.sshserver.auth import NoSuchPersonWithName
+from twisted.web import xmlrpc
+
+__all__ = [
+    "FakeAuthServerService",
+    "FakeVirtInfoService",
+    ]
+
+
+class FakeAuthServerService(xmlrpc.XMLRPC):
+    """A fake version of the Launchpad authserver service."""
+
+    def __init__(self):
+        xmlrpc.XMLRPC.__init__(self)
+        self.keys = defaultdict(list)
+
+    def addSSHKey(self, username, public_key_path):
+        with open(public_key_path, "r") as f:
+            public_key = f.read()
+        kind, keytext, _ = public_key.split(" ", 2)
+        if kind == "ssh-rsa":
+            keytype = "RSA"
+        elif kind == "ssh-dss":
+            keytype = "DSA"
+        else:
+            raise Exception("Unrecognised public key type %s" % kind)
+        self.keys[username].append((keytype, keytext))
+
+    def xmlrpc_getUserAndSSHKeys(self, username):
+        if username not in self.keys:
+            raise NoSuchPersonWithName(username)
+        return {
+            "id": hash(username) % (2 ** 31),
+            "name": username,
+            "keys": self.keys[username],
+            }
+
+
+class FakeVirtInfoService(xmlrpc.XMLRPC):
+    """A trivial virt information XML-RPC service.
+
+    Translates a path to its SHA-256 hash. The repo is writable if the
+    path is prefixed with '/+rw'
+    """
+
+    def __init__(self, *args, **kwargs):
+        xmlrpc.XMLRPC.__init__(self, *args, **kwargs)
+        self.require_auth = False
+        self.translations = []
+        self.authentications = []
+        self.push_notifications = []
+        self.ref_permissions_checks = []
+        self.ref_permissions = {}
+        self.ref_permissions_as_dict = False
+
+    def xmlrpc_translatePath(self, pathname, permission, auth_params):
+        if self.require_auth and 'user' not in auth_params:
+            raise xmlrpc.Fault(3, "Unauthorized")
+
+        self.translations.append((pathname, permission, auth_params))
+        writable = False
+        if pathname.startswith('/+rw'):
+            writable = True
+            pathname = pathname[4:]
+
+        if permission != b'read' and not writable:
+            raise xmlrpc.Fault(2, "Repository is read-only")
+        return {'path': hashlib.sha256(pathname).hexdigest()}
+
+    def xmlrpc_authenticateWithPassword(self, username, password):
+        self.authentications.append((username, password))
+        return {'user': username}
+
+    def xmlrpc_notify(self, path):
+        self.push_notifications.append(path)
+
+    def xmlrpc_checkRefPermissions(self, path, ref_paths, auth_params):
+        self.ref_permissions_checks.append((path, ref_paths, auth_params))
+        return self.ref_permissions
--- turnip/pack/tests/test_functional.py
+++ turnip/pack/tests/test_functional.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015 Canonical Ltd.  This software is licensed under the
+# Copyright 2015-2018 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 from __future__ import (
@@ -9,7 +9,6 @@ from __future__ import (
     )
 
 import base64
-from collections import defaultdict
 import hashlib
 import io
 import os
@@ -32,7 +31,6 @@ from fixtures import (
     EnvironmentVariable,
     TempDir,
     )
-from lazr.sshserver.auth import NoSuchPersonWithName
 from pygit2 import GIT_OID_HEX_ZERO
 from testtools import TestCase
 from testtools.content import text_content
@@ -62,78 +60,11 @@ from turnip.pack.hookrpc import (
     )
 from turnip.pack.http import SmartHTTPFrontendResource
 from turnip.pack.ssh import SmartSSHService
+from turnip.pack.tests.fake_servers import (
+    FakeAuthServerService,
+    FakeVirtInfoService,
+    )
 from turnip.version_info import version_info
-
-
-class FakeAuthServerService(xmlrpc.XMLRPC):
-    """A fake version of the Launchpad authserver service."""
-
-    def __init__(self):
-        xmlrpc.XMLRPC.__init__(self)
-        self.keys = defaultdict(list)
-
-    def addSSHKey(self, username, public_key_path):
-        with open(public_key_path, "r") as f:
-            public_key = f.read()
-        kind, keytext, _ = public_key.split(" ", 2)
-        if kind == "ssh-rsa":
-            keytype = "RSA"
-        elif kind == "ssh-dss":
-            keytype = "DSA"
-        else:
-            raise Exception("Unrecognised public key type %s" % kind)
-        self.keys[username].append((keytype, keytext))
-
-    def xmlrpc_getUserAndSSHKeys(self, username):
-        if username not in self.keys:
-            raise NoSuchPersonWithName(username)
-        return {
-            "id": hash(username) % (2 ** 31),
-            "name": username,
-            "keys": self.keys[username],
-            }
-
-
-class FakeVirtInfoService(xmlrpc.XMLRPC):
-    """A trivial virt information XML-RPC service.
-
-    Translates a path to its SHA-256 hash. The repo is writable if the
-    path is prefixed with '/+rw'
-    """
-
-    def __init__(self, *args, **kwargs):
-        xmlrpc.XMLRPC.__init__(self, *args, **kwargs)
-        self.require_auth = False
-        self.translations = []
-        self.authentications = []
-        self.push_notifications = []
-        self.ref_permissions_checks = []
-        self.ref_permissions = {}
-
-    def xmlrpc_translatePath(self, pathname, permission, auth_params):
-        if self.require_auth and 'user' not in auth_params:
-            raise xmlrpc.Fault(3, "Unauthorized")
-
-        self.translations.append((pathname, permission, auth_params))
-        writable = False
-        if pathname.startswith('/+rw'):
-            writable = True
-            pathname = pathname[4:]
-
-        if permission != b'read' and not writable:
-            raise xmlrpc.Fault(2, "Repository is read-only")
-        return {'path': hashlib.sha256(pathname).hexdigest()}
-
-    def xmlrpc_authenticateWithPassword(self, username, password):
-        self.authentications.append((username, password))
-        return {'user': username}
-
-    def xmlrpc_notify(self, path):
-        self.push_notifications.append(path)
-
-    def xmlrpc_checkRefPermissions(self, path, ref_paths, auth_params):
-        self.ref_permissions_checks.append((path, ref_paths, auth_params))
-        return self.ref_permissions
 
 
 class FunctionalTestMixin(object):
