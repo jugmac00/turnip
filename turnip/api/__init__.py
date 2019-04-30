38a2925e7ed728c91ef54c42d41d9a36539e3e87
Colin Watson <cjwatson@canonical.com>
2019-04-30 17:15:14 +0100

    Raise soft RLIMIT_NOFILE as high as possible
    The previous code to raise the soft RLIMIT_NOFILE actually had the
    effect of lowering it if it was already greater than 4096, which wasn't
    quite what I'd intended.  Just raise it all the way to the hard limit
    instead.
    
    LP: #1801392

--- turnip/api/__init__.py
+++ turnip/api/__init__.py
@@ -10,9 +10,9 @@ from pyramid.config import Configurator
 
 def main(global_config, **settings):
     # Allow slack for lots of open pack files.
-    _, hard_nofile = resource.getrlimit(resource.RLIMIT_NOFILE)
-    resource.setrlimit(
-        resource.RLIMIT_NOFILE, (min(4096, hard_nofile), hard_nofile))
+    soft_nofile, hard_nofile = resource.getrlimit(resource.RLIMIT_NOFILE)
+    if soft_nofile < hard_nofile:
+        resource.setrlimit(resource.RLIMIT_NOFILE, (hard_nofile, hard_nofile))
 
     config = Configurator(settings=settings)
     config.include("cornice")
