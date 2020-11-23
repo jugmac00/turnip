28dc1a5108d3c7e9c305db61d6741dc08aac404d
Thiago F Pappacena <thiago.frazao.pappacena@canonical.com>
2020-11-23 18:39:18 -0300

    Py3 compat for HTTP protocol version header

--- turnip/pack/http.py
+++ turnip/pack/http.py
@@ -84,7 +84,7 @@ def fail_request(request, message, code=http.INTERNAL_SERVER_ERROR):
 
 def get_protocol_version_from_request(request):
     version_header = request.requestHeaders.getRawHeaders(
-        'git-protocol', [b'version=0'])[0]
+        b'git-protocol', [b'version=0'])[0]
     try:
         return six.ensure_binary(version_header).split(b'version=', 1)[1]
     except IndexError:
